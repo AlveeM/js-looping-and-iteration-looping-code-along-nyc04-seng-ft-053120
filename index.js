d080930455ea53b259c01a668a7ab82ebd275c51
AlveeM <alvee.akand@outlook.com>
2020-08-01 15:01:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, eventName) {
+  const resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    resultArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return resultArr;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
